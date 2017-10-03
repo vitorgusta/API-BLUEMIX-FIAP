3a8d1d83b8b31e14bbba7878f9ee9b91c0a360d3
vitorgusta <vitor.garcia72292@outlook.com>
2017-10-03 02:56:14 -0300

    commit 03/10/2017

--- Api-Fiap-Bluemix/Controllers/HomeController.cs
+++ Api-Fiap-Bluemix/Controllers/HomeController.cs
@@ -5,6 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using dotnetBluemix;
+using IBM.WatsonDeveloperCloud.Conversation.v1;
+using IBM.WatsonDeveloperCloud.Conversation.v1.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Api_Fiap_Bluemix.Controllers
 {
@@ -19,22 +23,45 @@ namespace Api_Fiap_Bluemix.Controllers
             return new string[] { "value1", "value2" };
         }
 
-        // GET: api/Home/5
-        [HttpGet("{id}", Name = "Get")]
-        public Object Get(string msg)
-        {
-            ConversationHelper helper = new ConversationHelper("62a2a278-894d-42eb-999d-5aedaad8218d", "dcbf3a3f-2e0b-4ef5-943f-5923b98daa34", "RCKvyGkBkkj1");
-            var res = helper.GetResponse(msg).GetAwaiter().GetResult();
-            return res;
+        //// GET: api/Home/5
+        //[HttpGet("{id}", Name = "Get")]
+        //public Object Get(string msg)
+        //{
+        //    ConversationHelper helper = new ConversationHelper("62a2a278-894d-42eb-999d-5aedaad8218d", "dcbf3a3f-2e0b-4ef5-943f-5923b98daa34", "RCKvyGkBkkj1");
+        //    //var res = helper.GetResponse(msg).GetAwaiter().GetResult();
+        //    //return res;
 
-        }
+        //}
         
         // POST: api/Home
         [HttpPost]
-        public void Post([FromBody]string value)
+        public string Post([FromBody] string input)
         {
-        }
+
+            Console.WriteLine("########" + input);
+            // ConversationHelper helper = new ConversationHelper("1758047a-5adf-413a-8d05-a367cf729fcb", "dcbf3a3f-2e0b-4ef5-943f-5923b98daa34", "RCKvyGkBkkj1");
+            //var res = helper.GetResponse(value).GetAwaiter().GetResult();
+            ConversationService conversationService = new ConversationService("dcbf3a3f-2e0b-4ef5-943f-5923b98daa34", "RCKvyGkBkkj1", ConversationService.CONVERSATION_VERSION_DATE_2017_05_26);
+
+
+
+
+
         
+       
+            MessageRequest messageRequest = new MessageRequest()
+            {
+                Input = new InputData()
+                {
+                    Text = input
+                }
+            };
+            var result = conversationService.Message("1758047a-5adf-413a-8d05-a367cf729fcb", messageRequest);
+            Console.WriteLine(string.Format("result: {0}", JsonConvert.SerializeObject(result, Formatting.Indented)));
+            return JsonConvert.SerializeObject(result, Formatting.Indented);
+
+        }
+
         // PUT: api/Home/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
--- Api-Fiap-Bluemix/infra/ConversationHelper.cs
+++ Api-Fiap-Bluemix/infra/ConversationHelper.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using IBM.WatsonDeveloperCloud.Conversation.v1;
+using IBM.WatsonDeveloperCloud.Conversation.v1.Model;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -11,30 +14,50 @@ namespace dotnetBluemix
 
     public class ConversationHelper
     {
-        private readonly string _Server;
-        private readonly NetworkCredential _NetCredential;
+        //private readonly string _Server;
+       // private readonly NetworkCredential _NetCredential;
         public ConversationHelper(string workSpaceId, string userId, string password)
         {
-            _Server = string.Format("https://gateway.watsonplatform.net/conversation/api/v1/workspaces/{0}/message?version={1}", workSpaceId, DateTime.Today.ToString("yyyy-MM-dd"));
-            _NetCredential = new NetworkCredential(userId, password);
+            //_Server = string.Format("https://gateway.watsonplatform.net/conversation/api/v1/workspaces/{0}/message?version={1}", workSpaceId, DateTime.Today.ToString("yyyy-MM-dd"));
+            //_NetCredential = new NetworkCredential(userId, password);
+            ConversationService conversationService = new ConversationService(userId, password,ConversationService.CONVERSATION_VERSION_DATE_2017_05_26);
             
-        }
-        public async Task<string> GetResponse(string input, string context = null)
-        {
-            string req = null;
-            if (string.IsNullOrEmpty(context)) req = "{\"input\": {\"text\": \"" + input + "\"}, \"alternate_intents\": true}";
-            else req = "{\"input\": {\"text\": \"" + input + "\"}, \"alternate_intents\": true}, \"context\": \"" + context + "\"";
-            using (var handler = new HttpClientHandler
+
+            MessageRequest messageRequest = new MessageRequest()
             {
-                Credentials = _NetCredential
-            })
-            using (var client = new HttpClient(handler))
-            {
-                var cont = new HttpRequestMessage();
-                cont.Content = new StringContent(req.ToString(), Encoding.UTF8, "application/json");
-                var result = await client.PostAsync(_Server, cont.Content);
-                return await result.Content.ReadAsStringAsync();
-            }
+                Input = new InputData()
+                {
+                    Text = "Oi"
+                }
+            };
+            var result = conversationService.Message(workSpaceId, messageRequest);
+            Console.WriteLine(string.Format("result: {0}", JsonConvert.SerializeObject(result,Formatting.Indented)));
+
         }
+        //public async Task<string> GetResponse(string input, string context = null)
+        //{
+        //    input = "OI";
+        //    string req = null;
+        //    if (string.IsNullOrEmpty(context)) req = "{\"input\": {\"text\": \"" + input + "\"}, \"alternate_intents\": true}";
+        //    else req = "{\"input\": {\"text\": \"" + input + "\"}, \"alternate_intents\": true}, \"context\": \"" + context + "\"";
+        //    using (var handler = new HttpClientHandler
+        //    {
+        //        Credentials = _NetCredential
+        //    })
+        //    using (var client = new HttpClient(handler))
+        //    {
+        //        var cont = new HttpRequestMessage();
+        //        cont.Content = new StringContent(req.ToString(), Encoding.UTF8, "application/json");
+        //        Console.WriteLine("AQUI", req.ToString());
+        //        var result = await client.PostAsync(_Server, cont.Content);
+        //        //var stringResponse = await result.Content.ReadAsStringAsync();
+        //        //var posts = JsonConvert.DeserializeObject<string>(stringResponse);
+        //        //Console.WriteLine("AQUI", posts);
+        //        return await result.Content.ReadAsStringAsync();
+        //    }
+        //}
+
+
+
     }
 }
